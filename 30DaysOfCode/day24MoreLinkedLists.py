6d9691c4d927f8ca345047a829f884de4056aceb
patnir <rahul20patni@gmail.com>
2016-06-02 13:05:36 -0400

    Add day 24 solution in python

---
+++ 30DaysOfCode/day24MoreLinkedLists.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+class Solution:
+    def insert(self,head,data):
+        p = Node(data)        
+        if head==None:
+            head=p
+        elif head.next==None:
+            head.next=p
+        else:
+            start=head
+            while(start.next!=None):
+                 start=start.next
+            start.next=p
+        return head 
+    def display(self,head):
+        current = head
+        while current:
+            print current.data,
+            current = current.next	
+    def removeDuplicates(self,head):
+        #Write your code here
+        if (head == None):
+            return
+        last = head.data
+        head = head.next
+        while (head != None):
+            if (head.data == last):
+                head = head.next
+            else:
+                print last,
+                last = head.data
+        print last
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)   
+head=mylist.removeDuplicates(head)
+mylist.display(head);
